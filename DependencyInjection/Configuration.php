883a92e612ca9e6369abd00d7bb5bf70e9758358
Kaelin Jacobson <kaelinjacobson@gmail.com>
2013-06-14 14:06:16 -0700

    Fixed namespace issue

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace API\ChannelAdvisorBundle\DependencyInjection;
+namespace KMJ\ChannelAdvisorBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
