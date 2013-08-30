8a6f47032748eaae8c4d36c57bbc64ed7ed03b75
Kaelin Jacobson <kaelinjacobson@gmail.com>
2013-08-30 09:41:05 -0700

    Added traits… commit issue prevented them from being committed last time

---
+++ Traits/AdvancedElementTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace KMJ\ChannelAdvisorBundle\Traits;
+
+/**
+ *
+ * @author kaelinjacobson
+ */
+trait AdvancedElementTrait {
+
+    public function __construct($obj = null) {
+        if ($obj != null)
+            $this->load($obj);
+    }
+
+    public function load($obj) {
+        foreach ($obj as $key => $value) {
+            $key = lcfirst($key);
+            
+            if (property_exists($this, $key)) {
+                $this->$key = $this->formatValue($key, $value);
+            } else if (property_exists($this, ucfirst($key)))  {
+                $key = ucfirst($key);
+                $this->$key = $this->formatValue($key, $value);
+            } else {
+                $class = get_class($this);
+                echo "no property: {$class} {$key}".PHP_EOL;
+            }
+        }
+    }
+
+    abstract public function formatValue($key, $value);
+}
---
+++ Traits/SimpleElementTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace KMJ\ChannelAdvisorBundle\Traits;
+
+/**
+ * Description of simpleElementTrait
+ *
+ * @author kaelinjacobson
+ */
+trait SimpleElementTrait {
+    
+    public function __construct($obj = null) {
+        if ($obj != null) 
+            $this->load($obj);
+    }
+    
+    public function load($obj) {
+        foreach ($obj as $key => $value) {
+            $key = lcfirst($key);
+
+            if (property_exists($this, $key)) {
+                $this->$key = (string) $value;
+            } else if (property_exists($this, ucfirst($key)))  {
+                $key = ucfirst($key);
+                $this->$key = (string) $value;
+            } else {
+                $class = get_class($this);
+                echo "no property: {$class} {$key}".PHP_EOL;
+            }
+        }
+    }
+}
